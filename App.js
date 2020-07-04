90ce10cd1d7fff5498df9389682205440d1a6a36
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-07-04 17:14:01 +0900

    Buttonコンポーネントサンプル

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render(){
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
+        <Button
+          onPress={() => {alert("A Button onPress")}}
+          title="ボタンA"
+        />
+        <Button
+          color="red"
+          onPress={() => {alert("B Button onPress")}}
+          title="ボタンB"
+        />
+        <Button
+          disabled={true}
+          onPress={() => {alert("C Button onPress")}}
+          title="ボタンC"
+        />
       </View>
     )
   }
