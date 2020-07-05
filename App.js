5582f2dc788d9f2c4386db6b51f494f5b651585d
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-07-05 15:20:19 +0900

    Redux/Ducksサンプルコード

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from "react-redux";
+import Home from "./src/containers/Home";
+import configureStore from "./src/configureStore";
+
+const store = configureStore();
 
 export default class App extends React.Component {
-  render(){
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    )
+      <Provider store={store}>
+        <Home />
+      </Provider>
+    );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
