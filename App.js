9084ef10c595340b2f93daff7f8ed4cb56712c95
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-07-05 15:06:28 +0900

    BottomTabNavigatorサンプルコード

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Stack = createStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "一覧",
+  };
+  render() {
+    return (
+      <Button
+        title="詳細画面へ"
+        onPress={() => this.props.navigation.navigate("Detail")}
+      />
+    );
+  }
+}
+
+class DetailScreen extends React.Component {
+  static navigationOptions = {
+    title: "詳細",
+  };
+  render() {
+    return (
+      <Button
+        title="一覧画面へ"
+        onPress={() => this.props.navigation.goBack()}
+      />
+    );
+  }
+}
+
+function BottomNavigation() {
+  return (
+    <BottomTab.Navigator>
+      <BottomTab.Screen name="Home" component={HomeScreen} />
+      <BottomTab.Screen name="Detail" component={DetailScreen} />
+    </BottomTab.Navigator>
+  );
+}
 
 export default class App extends React.Component {
   render(){
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator >
+          <Stack.Screen name="App" component={BottomNavigation} />
+        </Stack.Navigator>
+      </NavigationContainer>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
