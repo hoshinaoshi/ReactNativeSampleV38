5b0470a8c27b1b5362c5d4ba830fe2de8e0f7022
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-07-05 15:36:25 +0900

    Redux-Sagaサンプルコード

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,22 @@
 import { createStore as reduxCreateStore, applyMiddleware, combineReducers } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas"
+
 import homeReducer from "./modules/Home";
+
+const sagaMiddleware = createSagaMiddleware()
 
 export default function createStore() {
   const store = reduxCreateStore(
     combineReducers({
       home: homeReducer,
     }),
+    applyMiddleware(
+      sagaMiddleware,
+      logger
+    ),
   );
-
+  sagaMiddleware.run(rootSaga)
   return store;
 }
--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,5 +1,13 @@
+import { call, put, takeLatest } from "redux-saga/effects"
+import axios from "axios";
+
+export const APIClient = axios.create({
+  baseURL: "http://localhost"
+});
+
 // action type
 const INCREMENT = "HOME_INCREMENT";
+const AFTER_INCREMENT = "HOME_AFTER_INCREMENT";
 const DECREMENT = "HOME_DECREMENT";
 
 // action-creator
@@ -7,9 +15,34 @@ export function increment() {
   return { type: INCREMENT };
 }
 
+export function afterIncrement() {
+  return { type: AFTER_INCREMENT };
+}
+
 export function decrement() {
   return { type: DECREMENT };
 }
+
+// redux-saga
+function executeAPI() {
+  let error;
+  let response;
+  /* axiosを使用してAPI通信
+  return APIClient.get("/users", {}).then((response) => {
+    return { response, error }
+  })
+  */
+  return { response, error }
+}
+
+function* incrementCount(action) {
+  const { response, error } = yield call(executeAPI);
+  yield put({type: AFTER_INCREMENT});
+}
+
+export const HomeSagas = [
+  takeLatest(INCREMENT, incrementCount),
+]
 
 // reducer
 const initialState = {
