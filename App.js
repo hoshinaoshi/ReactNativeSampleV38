3b9781bee58e870287743d08254ece72c507066e
Naoshi Hoshi <hoshinaoshi@gmail.com>
2020-07-10 19:28:56 +0900

    FirebaseAnalyticsサンプルコード

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+import * as Analytics from 'expo-firebase-analytics';
 
 export default class App extends React.Component {
+  async componentDidMount(){
+    // デバッグ用の設定
+    if(Constants.manifest.releaseChannel != "production"){
+      Analytics.setDebugModeEnabled(true);
+    }
+
+    await Analytics.logEvent('componentDidMount', {
+      name: 'react events',
+      screen: 'app',
+      purpose: 'Opens the App',
+    });
+  }
   render(){
     return (
       <View style={styles.container}>
